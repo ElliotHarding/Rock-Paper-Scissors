2b0ec93c9ae86d57f7eb06d790cf90387175ad5b
Elliot <elliot.one.harding@gmail.com>
2022-04-09 20:19:47 +0100

    progess

--- Rock-Paper-Scissors/mainwindow.cpp
+++ Rock-Paper-Scissors/mainwindow.cpp
@@ -13,3 +13,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+GameObject::GameObject(QWidget *parent, GameObjectType goType) : QWidget(parent),
+    m_type(goType)
+{
+}
+
+void GameObject::setType(GameObjectType type)
+{
+    m_type = type;
+}
+
+GameObjectType GameObject::getType()
+{
+    return m_type;
+}
+
+QMap<QPair<GameObjectType, GameObjectType>, GameObjectType> collisionResult = {
+    {QPair<GameObjectType, GameObjectType>(GO_ROCK, GO_ROCK), GO_ROCK},
+    {QPair<GameObjectType, GameObjectType>(GO_ROCK, GO_PAPER), GO_PAPER},
+    {QPair<GameObjectType, GameObjectType>(GO_ROCK, GO_SCISSORS), GO_ROCK},
+    {QPair<GameObjectType, GameObjectType>(GO_PAPER, GO_ROCK), GO_PAPER},
+    {QPair<GameObjectType, GameObjectType>(GO_PAPER, GO_PAPER), GO_PAPER},
+    {QPair<GameObjectType, GameObjectType>(GO_PAPER, GO_SCISSORS), GO_SCISSORS},
+    {QPair<GameObjectType, GameObjectType>(GO_SCISSORS, GO_ROCK), GO_ROCK},
+    {QPair<GameObjectType, GameObjectType>(GO_SCISSORS, GO_PAPER), GO_SCISSORS},
+    {QPair<GameObjectType, GameObjectType>(GO_SCISSORS, GO_SCISSORS), GO_SCISSORS}
+};
+
+void GameObject::checkCollided(GameObject *other)
+{
+    if(geometry().intersects(other->geometry()))
+    {
+        m_type = collisionResult[QPair<GameObjectType, GameObjectType>(m_type, other->getType())];
+    }
+}
--- Rock-Paper-Scissors/mainwindow.h
+++ Rock-Paper-Scissors/mainwindow.h
@@ -7,6 +7,31 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+enum GameObjectType
+{
+    GO_ROCK,
+    GO_PAPER,
+    GO_SCISSORS
+};
+
+class GameObject : public QWidget
+{
+    Q_OBJECT
+
+public:
+    GameObject(QWidget *parent, GameObjectType goType);
+
+    void setType(GameObjectType type);
+    GameObjectType getType();
+
+    void checkCollided(GameObject* other);
+
+private:
+    void paintEvent(QPaintEvent* paintEvent) override;
+
+    GameObjectType m_type;
+};
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
